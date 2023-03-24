f5b89de800a3f293be21e787a5d13f761285b8ca
Jan Kowalski <kowalski@gmail.com>
2023-03-24 21:57:28 +0100

    Revert "Modification 2"

--- src/main/java/pl/zajavka/case1/Dog.java
+++ src/main/java/pl/zajavka/case1/Dog.java
@@ -2,6 +2,7 @@ package pl.zajavka.case1;
 
 public class Dog extends Animal {
     private String name;
+    private String name;
 
     public void sniff() {
         System.out.println("I'm soo sniffing!");
